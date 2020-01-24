32893584bd78614a431b70d9e33352806e51c5bd
claytonweaver <cweaver@cbctrenton.com>
2020-01-24 08:25:26 -0500

    Powers Table 1.0

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Lab_Four_Powers_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool resume = true;
+            while (resume)
+            {
+                int userInput = int.Parse(GetUserInput("Enter the value you would like the table to go to."));
+
+                for (int i = 1; i < userInput + 1; i++)
+                {
+                    Console.Write($"Number: |{i}|");
+                    SquareTo(i);
+                    CubeTo(i);
+                    Console.WriteLine();
+                }
+                    Console.WriteLine("Would you like to go again? (Y/N)");
+                    char userKey = Console.ReadLine().ToUpper()[0];
+                    resume = AskToContinue(userKey);
+
+            }
+        }
+
+        static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine();
+            return value;
+        }
+
+        static void SquareTo(int num)
+        {
+            int squared = (num * num);
+            Console.Write($" Squared: |{squared}|");
+        }
+
+        static void CubeTo(int num)
+        {
+            int cubed = (num * num * num);
+            Console.Write($" Cubed: |{cubed}|");
+        }
+
+        static bool AskToContinue(char userKey)
+        {
+            if (userKey == 'Y')
+            {
+                return true;
+            }
+            else if (userKey == 'N')
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input.");
+                return false;
+            }
+        }
+    }
+}
+
+
---
+++ RoughDraft.cs
@@ -0,0 +1,90 @@
+﻿/*
+using System;
+
+namespace Lab_Four_Powers_Table
+{
+    class RoughDraft
+    {
+        static void Main2(string[] args)
+        {
+
+            bool resume = true;
+            while (resume)
+            {
+                int userInput = int.Parse(GetUserInput("Enter the value you would like the table to go to."));
+
+                SquareTo(userInput);
+
+                Console.WriteLine();
+
+                CubeTo(userInput);
+
+                Console.WriteLine();
+
+                char userKey = Console.ReadLine().ToUpper()[0];
+
+                resume = AskToContinue(userKey);
+
+            }
+
+
+        }
+
+
+        static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string value = Console.ReadLine();
+            return value;
+
+        }
+
+        static void SquareTo(int max)
+        {
+            Console.Write("Squared: ");
+            for (int i = 1; i < max + 1; i++)
+            {
+                int squared = (i * i);
+
+                Console.Write($" |{squared}| ");
+
+            };
+
+        }
+        static void CubeTo(int max)
+        {
+            Console.Write("Cubed: ");
+            for (int i = 1; i < max + 1; i++)
+            {
+
+                int cubed = (i * i * i);
+
+                Console.Write($" |{cubed}| ");
+            }
+        }
+        static bool AskToContinue(char userKey)
+        {
+
+            Console.WriteLine("Would you like to go again? (Y/N)");
+
+            
+            if (userKey == 'Y')
+            {
+                return true;
+            }
+            else if (userKey == 'N')
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Not a valid input.");
+                return false;
+            }
+
+
+        }
+    }
+
+}
+*/
